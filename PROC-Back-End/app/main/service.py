f871e516545a2d1d878603202887423f9d6e48b5
SERGIOPILLISA <55695959+SERGIOPILLISA@users.noreply.github.com>
2019-10-21 14:05:52 -0500

    Fin experimento

--- PROC-Back-End/app/main/service.py
+++ PROC-Back-End/app/main/service.py
@@ -83,6 +83,11 @@ def get_size_by_id(_id):
     size_serializer = SizeSerializer()
     return size_serializer.jsonify(size) if size else Response(status=404)
 
+@urls.route('/size', methods=GET)
+def get_size():
+    result = get_all(Size, SizeSerializer)
+    return jsonify(result)
+
 
 # Order Routes
 
@@ -136,6 +141,6 @@ def get_orders():
 
 @urls.route('/order/id/<_id>', methods=GET)
 def get_order_by_id(_id):
-    order = Order()
+    order = Order.query.get(_id)
     order_serializer = OrderSerializer()
-    return order_serializer.jsonify({}) if order else Response(status=404)
+    return order_serializer.jsonify(order) if order else Response(status=404)
