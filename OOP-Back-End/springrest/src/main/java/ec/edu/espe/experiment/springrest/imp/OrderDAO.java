f4e90808823dc4ded423bf9fbfdb6b3c8dc3706c
sdvivas <santi.viv@hotmail.com>
2019-10-23 13:11:55 -0500

    Update OrderDAO.java

--- OOP-Back-End/springrest/src/main/java/ec/edu/espe/experiment/springrest/imp/OrderDAO.java
+++ OOP-Back-End/springrest/src/main/java/ec/edu/espe/experiment/springrest/imp/OrderDAO.java
@@ -72,17 +72,23 @@ public class OrderDAO implements IOrderDAO {
         try {
             DBOrder dbOrder = new DBOrder();
             dbOrder.setName(entity.getClient_name());
-            /*dbOrder.setAddress(entity.getClient_address());
+            dbOrder.setAddress(entity.getClient_address());
             dbOrder.setDni(entity.getClient_dni());
             
-            dbOrder.setPhone(entity.getClient_phone());*/
+            dbOrder.setPhone(entity.getClient_phone());
             dbOrder.setDate(new Date());
             dbOrder.setSize(repoSize.findById(entity.getSize()).get());
             repoOrder.save(dbOrder);
             repoOrder.flush();
+            Float total_price = 0f;
+            
             List<Ingredient> list_ingredient = daoDetail.post(dbOrder.getId(), entity.getIngredients());
             Size size = daoSize.get(dbOrder.getSize().getId());
-            Float total_price = 0f;
+            
+            for (int i = 0 ; i < list_ingredient.size(); i++){
+                total_price = list_ingredient.get(i).getPrice() + total_price;
+            }
+            total_price = total_price + size.getPrice();
             
             dbOrder.setTotal(total_price);
             repoOrder.save(dbOrder);
