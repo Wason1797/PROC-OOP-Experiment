2905d4fc5953c999c7d06f7408f5edbbf4c8b72b
Anshelo <anshelopu911@gmail.com>
2019-10-23 13:50:10 -0500

    Taller

--- PROC-Back-End/app/main/functions.py
+++ PROC-Back-End/app/main/functions.py
@@ -9,8 +9,8 @@ def get_all(Model, Serializer):
 def calculate_order_price(order, ingredients):
 
     size_price = order.size.price
-    sum(ingredient.price for ingredient in ingredients)
-    return round(size_price, 2)
+    total_price = size_price + sum(ingredient.price for ingredient in ingredients)
+    return round(total_price, 2)
 
 
 def check_required_keys(keys: tuple, _element: dict):
