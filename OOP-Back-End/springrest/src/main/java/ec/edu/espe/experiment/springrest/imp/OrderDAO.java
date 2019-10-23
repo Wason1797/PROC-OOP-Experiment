5380ce7a8715fea95fef88ae2579dc262e6e3c20
Carlos Alexis <cacabascango1@espe.edu.ec>
2019-10-23 15:56:13 -0500

    Forma A realizado 4 tareas

--- OOP-Back-End/springrest/src/main/java/ec/edu/espe/experiment/springrest/imp/OrderDAO.java
+++ OOP-Back-End/springrest/src/main/java/ec/edu/espe/experiment/springrest/imp/OrderDAO.java
@@ -82,6 +82,7 @@ public class OrderDAO implements IOrderDAO {
             List<Ingredient> list_ingredient = daoDetail.post(dbOrder.getId(), entity.getIngredients());
             Size size = daoSize.get(dbOrder.getSize().getId());
 
+            
             Float total_price = 0f;
 
             dbOrder.setTotal(total_price);
