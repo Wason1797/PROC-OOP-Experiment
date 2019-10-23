734992b7859711599d827448e84e9e17e172c038
SERGIOPILLISA <55695959+SERGIOPILLISA@users.noreply.github.com>
2019-10-23 13:47:02 -0500

    Fin del examen

--- OOP-Back-End/springrest/src/main/java/ec/edu/espe/experiment/springrest/rest/RestOrderController.java
+++ OOP-Back-End/springrest/src/main/java/ec/edu/espe/experiment/springrest/rest/RestOrderController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -37,4 +38,10 @@ public class RestOrderController{
     public Order post(@RequestBody OrderEntityClient entity) {
        return dao.post(entity);
     }
+
+    @PutMapping
+    public Order put(@RequestBody OrderEntityClient entity) {
+        return dao.post(entity);
+     }
+ 
 }
