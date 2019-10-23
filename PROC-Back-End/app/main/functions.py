bd7de3c3eb8a0505063caa4cea458d71debc9492
Francisco Segura <33929643+panchosegura@users.noreply.github.com>
2019-10-23 13:34:43 -0500

    Solución Taller
    Corrección de Código con todos los puntos del material revisados y corregidos.

--- PROC-Back-End/app/main/functions.py
+++ PROC-Back-End/app/main/functions.py
@@ -9,8 +9,10 @@ def get_all(Model, Serializer):
 def calculate_order_price(order, ingredients):
 
     size_price = order.size.price
-    sum(ingredient.price for ingredient in ingredients)
-    return round(size_price, 2)
+    #sum(ingredient.price for ingredient in ingredients)
+    total_price=size_price+sum(ingredient.price for ingredient in ingredients)
+    #return round(size_price, 2)
+    return total_price
 
 
 def check_required_keys(keys: tuple, _element: dict):
